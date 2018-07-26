77aa0a334555235a2af8b8cb98a9b78a667ff093
sninivas143 <41734007+sninivas143@users.noreply.github.com>
2018-07-26 15:45:28 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class  test
+{
+  public static void main(String []args)
+  {
+   Scanner sc=new Scanner(System.in);
+   int a=sc.nextInt();
+   if(a>0)
+   {
+   System.out.println("positive");
+   }
+   }
+   }
