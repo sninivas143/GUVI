267f749434172935acb1eabaf5bb9dc8927e5588
k.srinivas <41734007+sninivas143@users.noreply.github.com>
2018-07-27 13:06:16 +0530

    Create Naturalnum.java

---
+++ Naturalnum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Naturalnum
+{
+  public static void main(String[] args)
+  {
+       int s=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+    {
+		s=s+i;
+System.out.println(s);
+}
+}
+}
