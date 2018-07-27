65fcb8cb2b635cc628a36769ac209e7767178324
k.srinivas <41734007+sninivas143@users.noreply.github.com>
2018-07-27 12:36:01 +0530

    Create Leapyear.java

---
+++ Leapyear.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.utill.*;
+class Leapyear
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    int year=sc.nextInt();
+            boolean flag = false;
+
+        if(year % 400 == 0)
+
+        {
+
+            flag = true;
+
+        }
+
+        else if (year % 100 == 0)
+
+        {
+
+            flag = false;
+
+        }
+
+        else if(year % 4 == 0)
+
+        {
+
+            flag = true;
+
+        }
+
+        else
+
+        {
+
+            flag = false;
+
+        }
+
+        if(flag)
+
+        {
+
+            System.out.println("Year "+year+" is a Leap Year");
+
+        }
+
+        else
+
+        {
+
+            System.out.println("Year "+year+" is not a Leap Year");
+
+        }
