eaa76cc9b6070059f607e36fbd432e7bfbabde86
k.srinivas <41734007+sninivas143@users.noreply.github.com>
2018-07-27 14:59:45 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Array
+{
+  public static void main(String[] args)
+  {
+       int s=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+    int m=sc.nextInt();
+		for(int i=1;i<=n;i++)
+    {
+		s=s+i;
+System.out.println(s);
+}
+}
+}
