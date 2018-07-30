5b42488c82d5343c8306b2591ab5864e377c4c1c
k.srinivas <41734007+sninivas143@users.noreply.github.com>
2018-07-30 15:00:04 +0530

    Create Math.java

---
+++ Math.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class Math
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    System.out.println((int)Math.pow(a,b));
+    }
+   }
