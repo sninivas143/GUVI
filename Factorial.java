6b45cf22b6101bc787d661bdbb198278495c0192
k.srinivas <41734007+sninivas143@users.noreply.github.com>
2018-08-02 14:25:23 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Factorial
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		 int fact=1;
+		for(int i=1;i<=n;i++)
+	{
+		fact=fact*i;
+	}
+	System.out.println(fact);
+	
+		
+}
+}
+
+	
