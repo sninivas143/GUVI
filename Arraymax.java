134018536a47710c39cb8a6635ad6b0a1ca8d98b
k.srinivas <41734007+sninivas143@users.noreply.github.com>
2018-08-08 12:29:51 +0530

    Create Arraymax.java

---
+++ Arraymax.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Arraymax
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	        int n, max;
+
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        int a[] = new int[n];
+
+        for(int i = 0; i < n; i++)
+
+        {
+
+            a[i] = s.nextInt();
+
+        }
+
+        max = a[0];
+
+        for(int i = 0; i < n; i++)
+
+        {
+
+            if(max < a[i])
+
+            {
+
+                max = a[i];
+
+            }
+
+        }
+
+        System.out.println(max);
+	}
+}
