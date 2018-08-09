70bbfd014485a07c76dde3a70aad2a04544b4482
k.srinivas <41734007+sninivas143@users.noreply.github.com>
2018-08-09 11:44:38 +0530

    Update Reversstring.java

--- Reversstring.java
+++ Reversstring.java
@@ -13,7 +13,7 @@ class Reversstring
         String str = sc.nextLine();
         for(int i=str.length()-1;i>=0;i--)
         {
-        	System.out.println(str.charAt(i));
+        	System.out.print(str.charAt(i));
         }
 	}
 }
