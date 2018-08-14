fc7260f0454ae3e038c67752ff48292d451680d5
k.srinivas <41734007+sninivas143@users.noreply.github.com>
2018-08-14 11:45:48 +0530

    Create Arraymin.java

---
+++ Arraymin.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Arraymin
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int small, size, i;
+       int arr[] = new int[50];
+       Scanner scan = new Scanner(System.in)
+       size = scan.nextInt();
+	          for(i=0; i<size; i++)
+       {
+           arr[i] = scan.nextInt();
+       }
+	         small = arr[0];
+	   
+       for(i=0; i<size; i++)
+       {
+           if(small > arr[i])
+           {
+               small = arr[i];
+           }
+            }
+	          System.out.print(small);
+	}
+}
